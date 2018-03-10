7da7dc2b0a01ed5f4cb4b01e824e028ea66f3204
Karen E Gutzman <keg827@fsm.northwestern.edu>
2018-03-09 18:32:17 -0600

    reworked javascript

--- assets/javascript/starwargame.js
+++ assets/javascript/starwargame.js
@@ -1,35 +1,101 @@
 $(document).ready(function() {
+let luke = {
+    "name": "luke", 
+    "health" : 200,
+    "multiplier" : 100,
+    "power" : function () {
+        return Math.floor(Math.random() * this.multiplier)
+    },
     
+};
 
-//variables
-let darthMaulDiv = $('#darthmaul');
-let charAttacksDiv = $('#attackcharacters');
+let darthMaul = {
+    "name": "darth maul", 
+    "health" : 300,
+    "multiplier" : 5,
+    "power" : function () {
+        return Math.floor(Math.random() * this.multiplier)
+    },
+};
 
-
-//Functions
-function moveAttacker(attacker) {
-    charAttacksDiv.append(attacker);
+function powerBoost(jedi){
+    jedi.multiplier++;
+    return jedi;
 }
 
-//Events
-
-//clicking Luke will move darth maul below
-
-$("#luke").on('click', function() {
-    alert('clicked luke');
-    moveAttacker(darthMaulDiv);
+// function called attack two jedi
+function attack(jediOne, jediTwo) {
+    let j1Power = jediOne.power();
+    let j2Power = jediTwo.power();
+    jediOne.health = jediOne.health - j2Power;
+    jediTwo.health = jediTwo.health - j1Power;
     
+    console.log(jediOne.health);
+    console.log(jediTwo.health);
+//power boost
+    powerBoost(jediOne);
+    console.log("m" + jediOne.multiplier); 
+
+//check health
+    if (isJediDead(jediOne) === true){
+        alert(jediOne.name + " is lost");
+    }
+    if (isJediDead(jediTwo) === true){
+        alert(jediTwo.name + " is lost");
+    }
+    
+    
+}
+
+function isJediDead(jedi) {
+    if(jedi.health <= 0){
+        return true; 
+    } 
+    return false;
+}
+
+
+
+$('#attackBtn').on('click', function(){
+    attack(luke, darthMaul);
 });
 
-resetGame() {
-    //reset vlaues
-    startGame();
-}
+// attack(luke, darthMaul)
 
-funciton playGame() {
-    if win {
-        resetGame();
-    }
-}
+// //variables
+// let darthMaulDiv = $('#darthmaul');
+// let charAttacksDiv = $('#attackcharacters');
+
+
+// //Functions
+// function moveAttacker(attacker) {
+//     charAttacksDiv.append(attacker);
+// }
+
+// //Events
+
+// //clicking Luke will move darth maul below
+
+// $("#luke").on('click', function() {
+//     alert('clicked luke');
+//     moveAttacker(darthMaulDiv);
+    
+// });
+
+// $("#attackBtn").on('click', function() {
+    
+    
+// });
+
+// resetGame() {
+//     //reset vlaues
+//     startGame();
+// }
+
+// funciton playGame() {
+//     if win {
+//         resetGame();
+//     }
+// }
 
 });
